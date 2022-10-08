f3bb476ccd6c4a2c6768aaa538f6b47d731ce320
Michelle Simoni <michelle.simoni779@gmail.com>
2022-10-08 12:48:08 -0700

    fix: wip
    a wip on what we have been working on

--- xtractopy_mod/xtracttopy_mod.py
+++ xtractopy_mod/xtracttopy_mod.py
@@ -8,6 +8,47 @@ import fsspec
 import matplotlib.pyplot as plt
 from datetime import datetime
 import logging
+from abc import ABC
+
+class logger(ABC):
+    """
+    Abstract base class for creating a logger for each class created
+    """
+    def _create_logger(self):
+        """
+        :return: private function that returns a logger
+        """
+        # Gets or creates a logger
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+        logger = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+        return logger
+
+class XArrayHelper(logger):
+    def __init__(self, max_long,min_long):
+        self.max_long = max_long
+        self.min_long = min_long
+        logger = self._create_logger()
+
+    def create_subset(self,large_data_set):
+        """
+        :param large_data_set: an Xarray of data
+        :return: will take the max long and min long used and find them  in the subset and just return the working data
+        """
+        subset_lon = (large_data_set.lon >= self.min_long) & (large_data_set.lon <= self.max_long)
+        subset_env_data = large_data_set.where(subset_lon, drop=True)
+        return subset_env_data
+
+    def flip_table(self):
+        """
+        goal is to make each col a row in a pandas or spark dataframe so we can analyze data
+        :return:
+        """
+        pass
+
+
+
+
 
 
 
